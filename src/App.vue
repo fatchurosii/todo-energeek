aa61e49cf0d6b6938a6a8717a220f4c555617525
Fatchur Rosi <fatchurr0198@gmail.com>
2024-10-02 13:06:13 +0700

    add update progress

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
             </div>
           </div>
           <!-- <div class="grid grid-flow-col gap-5"> -->
-            <div class="w-full overflow-x-auto">
+            <div class="w-full overflow-x-auto" v-if="todo == 1">
               <table class="min-w-full border-collapse table-auto">
                 <thead>
                   <tr class="text-sm leading-normal text-gray-700 uppercase">
@@ -28,12 +28,12 @@
                 <tbody class="text-sm font-light text-gray-600">
                   <template v-for="(task, index) in tasks" :key="index">
                     <tr class="text-center border-b border-gray-200 hover:bg-gray-100">
-                      <td class="px-6 py-3 text-left">{{ index + 1 }}</td>
+                      <td class="px-6 py-3 text-lg font-semibold text-left text-slate-700">{{ index + 1 }}</td>
                       <td class="px-6 py-3 text-left">
-                        <input type="text" class=" border border-gray-300 text-slate-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " v-model="task.projectName" />
+                        <input type="text" class=" border border-gray-300 text-slate-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 font-semibold " v-model="task.projectName" />
                       </td>
                       <td class="px-6 py-3 text-left">
-                        <input type="text" class=" border border-gray-300 text-slate-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " v-model="task.taskName" />
+                        <input type="text" class=" border border-gray-300 text-slate-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 font-semibold" v-model="task.taskName" />
                       </td>
                       <td class="px-6 py-3 text-left">
                         <!-- Add your action buttons here -->
@@ -49,26 +49,120 @@
                 <button class="px-4 py-2 font-bold text-white rounded bg-emerald-500 hover:bg-emerald-700" @click="addTask">
                   Tambah
                 </button>
-                <button class="px-4 py-2 font-bold text-white rounded bg-emerald-500 hover:bg-emerald-700" @click="generate">
-                  Generate
-                </button>
+              </div>
+              <div class="w-full py-5 overflow-auto" v-if="tasks.length > 0" >
+                <div class="bg-slate-100 shadow-[5px_5px_0px_0px] shadow-teal-500 p-5 rounded-md border border-teal-600">
+                  <div class="flex justify-between mb-5">
+                    <h1 class="text-2xl font-semibold text-dark">Generate {{todo == 1 ? 'Todo' : "Update Progress"}}</h1>
+                    <h1 class="px-4 py-2 font-medium text-white bg-teal-500 rounded cursor-pointer font-body text-md" @click="copyToClipboardTodo">Copy Text</h1>
+                  </div>
+                  <div class="p-5 border rounded bg-slate-200 border-slate-300" id="mytodo">
+                    <h1 class="text-lg font-semibold text-dark font-body">{{todo == 1 ? 'mytodolist:' : 'myprogresslist:'}}</h1>
+                    <template v-for="task in tasks" :key="task.id">
+                      <h1 class="text-lg font-semibold text-dark font-body">{{task.id}}. {{task.projectName}} - {{task.taskName}}</h1>
+                    </template>
+                  </div>
+                </div>
               </div>
             </div>
 
-            <div class="w-full py-5 overflow-auto" v-if="isGenerate">
-              <div class="bg-slate-100 shadow-[5px_5px_0px_0px] shadow-teal-500 p-5 rounded-md border border-teal-600">
-                <div class="flex justify-between mb-5">
-                  <h1 class="text-2xl font-semibold text-dark">Generate {{todo == 1 ? 'Todo' : "Update Progress"}}</h1>
-                  <h1 class="px-4 py-2 font-medium text-white bg-teal-500 rounded cursor-pointer font-body text-md" @click="copyToClipboard">Copy Text</h1>
+            <div class="w-full overflow-x-auto" v-if="todo == 0">
+              <div class="mb-10">
+     
+                <h1 class="text-lg font-semibold text-dark font-body">Task Progress</h1>
+                <table class="min-w-full mb-5 border-collapse table-auto">
+                  <thead>
+                    <tr class="text-sm leading-normal text-gray-700 uppercase">
+                      <th class="px-6 py-3 text-left">No</th>
+                      <th class="px-6 py-3 text-left">Project Name</th>
+                      <th class="px-6 py-3 text-left">Task</th>
+                      <th class="px-6 py-3 text-left">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody class="text-sm font-light text-gray-600">
+                    <template v-for="(updateTask, index) in updateTasks" :key="index">
+                      <tr class="text-center border-b border-gray-200 hover:bg-gray-100">
+                        <td class="px-6 py-3 text-lg font-semibold text-left text-slate-700">{{ index + 1 }}</td>
+                        <td class="px-6 py-3 text-left">
+                          <input type="text" class=" border border-gray-300 text-slate-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 font-semibold " v-model="updateTask.projectName" />
+                        </td>
+                        <td class="px-6 py-3 text-left">
+                          <input type="text" class=" border border-gray-300 text-slate-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 font-semibold" v-model="updateTask.taskName" />
+                        </td>
+                        <td class="px-6 py-3 text-left">
+                          <!-- Add your action buttons here -->
+                          <button class="px-4 py-2 font-bold text-white bg-red-500 rounded hover:bg-red-700" @click="deleteUpdateTask(updateTask.id)">
+                            Delete
+                          </button>
+                        </td>
+                      </tr>
+                    </template>
+                  </tbody>
+                </table>
+                <div class="flex justify-between mt-5">
+                  <button class="px-4 py-2 font-bold text-white rounded bg-emerald-500 hover:bg-emerald-700" @click="addUpdateTask">
+                    Tambah
+                  </button>
                 </div>
-                <div class="p-5 border rounded bg-slate-200 border-slate-300" id="mytodo">
-                  <h1 class="text-lg font-semibold text-dark font-body">{{todo == 1 ? 'mytodolist:' : 'myprogresslist:'}}</h1>
-                  <template v-for="task in tasks" :key="task.id">
-                    <h1 class="text-lg font-semibold text-dark font-body">{{task.id}}. {{task.projectName}} - {{task.taskName}}</h1>
+              </div>
+              <table class="min-w-full mb-5 border-collapse table-auto">
+                <thead>
+                  <tr class="text-sm leading-normal text-gray-700 uppercase">
+                    <th class="px-6 py-3 text-left">No</th>
+                    <th class="px-6 py-3 text-left">Project Name</th>
+                    <th class="px-6 py-3 text-left">Task</th>
+                    <th class="px-6 py-3 text-left">Action</th>
+                  </tr>
+                </thead>
+                <tbody class="text-sm font-light text-gray-600">
+                  <template v-for="(nextTask, index) in nextTasks" :key="index">
+                    <tr class="text-center border-b border-gray-200 hover:bg-gray-100">
+                      <td class="px-6 py-3 text-lg font-semibold text-left text-slate-700">{{ index + 1 }}</td>
+                      <td class="px-6 py-3 text-left">
+                        <input type="text" class=" border border-gray-300 text-slate-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 font-semibold " v-model="nextTask.projectName" />
+                      </td>
+                      <td class="px-6 py-3 text-left">
+                        <input type="text" class=" border border-gray-300 text-slate-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 font-semibold" v-model="nextTask.taskName" />
+                      </td>
+                      <td class="px-6 py-3 text-left">
+                        <!-- Add your action buttons here -->
+                        <button class="px-4 py-2 font-bold text-white bg-red-500 rounded hover:bg-red-700" @click="deleteNextTask(nextTask.id)">
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
                   </template>
+                </tbody>
+              </table>
+              <div class="flex justify-between mt-5">
+                <button class="px-4 py-2 font-bold text-white rounded bg-emerald-500 hover:bg-emerald-700" @click="addNextTask">
+                  Tambah
+                </button>
+              </div>
+              <div class="w-full py-5 overflow-auto" v-if="updateTasks.length > 0 && nextTasks.length > 0" >
+                <div class="bg-slate-100 shadow-[5px_5px_0px_0px] shadow-teal-500 p-5 rounded-md border border-teal-600">
+                  <div class="flex justify-between mb-5">
+                    <h1 class="text-2xl font-semibold text-dark">Generate {{todo == 1 ? 'Todo' : "Update Progress"}}</h1>
+                    <h1 class="px-4 py-2 font-medium text-white bg-teal-500 rounded cursor-pointer font-body text-md" @click="copyToClipboardUpdate">Copy Text</h1>
+                  </div>
+                  <div class="p-5 border rounded bg-slate-200 border-slate-300" id="myUpdate">
+                    <div class="mb-3">
+                      <h1 class="text-lg font-semibold text-dark font-body">myprogresslist: </h1>
+                      <template v-for="updateTask in updateTasks" :key="updateTask.id">
+                        <h1 class="text-lg font-semibold text-dark font-body">{{updateTask.id}}. {{updateTask.projectName}} - {{updateTask.taskName}}</h1>
+                      </template>
+                    </div>
+                    <div class="mb-3">
+                      <h1 class="text-lg font-semibold text-dark font-body">todo next: </h1>
+                      <template v-for="nextTask in nextTasks" :key="nextTask.id">
+                        <h1 class="text-lg font-semibold text-dark font-body">{{nextTask.id}}. {{nextTask.projectName}} - {{nextTask.taskName}}</h1>
+                      </template>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
+
             
           
        </div>
@@ -92,6 +186,9 @@ import {useToast} from 'vue-toastification'
  const tasks = ref([]);
  const isGenerate = ref(false);
  
+ const updateTasks = ref([]);
+ const nextTasks = ref([]);
+ 
  const switchBtn = () => {
    todo.value = !todo.value;
  }
@@ -105,18 +202,45 @@ import {useToast} from 'vue-toastification'
  const deleteTask = (id) => {
    tasks.value = tasks.value.filter((task) => task.id !== id)
  }
-
- const generate = () => {
-   if(tasks.value.length == 0){
-     toast.error('Data Tidak Boleh Kosong')
-     return;
-   }
-   isGenerate.value = !isGenerate.value
-   console.log(tasks.value)
+ const addUpdateTask = () => {
+  updateTasks.value.push({
+     id : updateTasks.value.length + 1,
+     projectName : '',
+     taskName : ''
+   })
  }
+ const deleteUpdateTask = (id) => {
+  updateTasks.value = updateTasks.value.filter((updateTasks) => updateTasks.id !== id)
+ }
+ const addNextTask = () => {
+  nextTasks.value.push({
+     id : nextTasks.value.length + 1,
+     projectName : '',
+     taskName : ''
+   })
+ }
+ const deleteNextTask = (id) => {
+  nextTasks.value = nextTasks.value.filter((nextTasks) => nextTasks.id !== id)
+ }
+ const copyToClipboardUpdate = () => {
+  const mytodoElement = document.getElementById('myUpdate');
+      const range = document.createRange();
+      range.selectNodeContents(mytodoElement);
+      const selection = window.getSelection();
+      selection.removeAllRanges();
+      selection.addRange(range);
 
- const copyToClipboard = () => {
-  const mytodoElement = document.getElementById('mytodo');
+      try {
+        document.execCommand('copy');
+        toast.success('Text copied to clipboard');
+      } catch (err) {
+       toast.error('Failed to copy text');
+      }
+
+      selection.removeAllRanges();
+ }
+ const copyToClipboardTodo = () => {
+      const mytodoElement = document.getElementById('mytodo');
       const range = document.createRange();
       range.selectNodeContents(mytodoElement);
       const selection = window.getSelection();
